2ba00a051560ea7a9cfd12cf95bc6617962b9b0b
Pete <pbianchi192@gmail.com>
2018-02-08 16:32:52 -0600

    First commit

---
+++ names.py
@@ -0,0 +1,34 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def names(arr):
+    for val in arr:
+        print val['first_name'], val['last_name']
+names(students)
+
+users = {
+    'Students': [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ],
+    'Instructors': [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+    ]
+}
+
+def studentsInstructors(dictionary):
+    for key, data in dictionary.items():
+        print key
+        # print data
+        count = 0
+        for val in data:
+            count += 1
+            print count, '-', val['first_name'], val['last_name'], '-', len(val['first_name'] + val['last_name'])
+studentsInstructors(users)
